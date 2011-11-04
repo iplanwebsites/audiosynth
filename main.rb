7fa341a3289794cdfd6c963c505b63e531ea1360
I Plan Websites <info@iplanwebsites.com>
2011-11-04 10:27:44 -0400

    first commit!

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'bundler/setup'
+require 'sinatra'
+require 'json'
+
+configure do
+	Rack::Mime::MIME_TYPES[".manifest"] = "text/cache-manifest"
+end
+
+get '/' do
+	# redirect '/clock.html'
+	erb :demo
+end
+
+
+get '/api/utc' do
+	t = Time.now 
+  "hello"
+  content_type 'text/html', :charset => 'utf-8'
+  t.utc.to_json
+end
