7b35d2e2cf29440ad6ec92a38bc75bd9e94a1bac
I Plan Websites <info@iplanwebsites.com>
2011-11-13 17:33:27 -0500

    tunings

--- public/js/guitar.js
+++ public/js/guitar.js
@@ -30,7 +30,8 @@ function drawNeck(nbCorde, cordeType, timeRatio, activeCorde){
 	// Draw each cordes:
 	var vibDirection =  Math.random()*2 - 1;
 	for (var c=0; c<nbCorde; c++) {
-	  thickness = ((maxDiam-minDiam) * (c/nbCorde))+minDiam;
+	  thickness = ((maxDiam-minDiam) * ((nbCorde-c) / nbCorde)) + minDiam;
+	  
 	  context.strokeStyle = '#4cc'; 
   	context.fillStyle = '#cee'; 
   	context.lineWidth   = thickness;
@@ -72,19 +73,58 @@ function update() {
 }
 
 auto_refresh = "";
-vib_duration = 5; //sec
-vib_fps = 30;
+vib_duration = 1; //sec
+vib_fps = 50;
+
+function getDiff(notes){
+  var corres = {
+    "A" : 0,
+    "A#" : 1,
+    "B" : 2,
+    "C" : 3,
+    "C#" : 4,
+    "D" : 5,
+    "D#" : 6,
+    "E" : 7,
+    "F" : 8,
+    "F#" : 9,
+    "G" : 10,
+    "G#" : 11,
+    "X" : "X",
+    "x" : "X",
+  }
+  var ar = notes.split(" ");
+   for (var i=0; i<ar.length; i++) { 
+     ar[i] = corres[ar[i]];
+  }
+  ar[0] -=12; //!! TODO: Find a clever way to ponder string basic value...
+  ar[4] +=12;
+  ar[5] +=12;
+  return ar
+}
+
+
+
+tuning = "E A D G B E";
+tuning_rel = getDiff(tuning); // returns an array...
+
+
+console.log('!!! '+ getDiff(tuning));
+
 function activateCorde(key){
   console.log(key +' + + + +');
   
   
   if (auto_refresh != "") {
+    clearInterval(auto_refresh);
+    a.pause();
       // already set
-      return;
+     // return;
   }
-    vib_count = 0;
+    vib_count = 0; //reset counter...
+    
     auto_refresh = setInterval(function() {
-      var ratio = (vib_count++ / vib_fps) / vib_duration;
+      var ratio = (vib_count++ / vib_fps) / vib_duration; // current / total-time
       if (ratio > 1) { 
         clearInterval(auto_refresh); 
         auto_refresh = "";
@@ -95,14 +135,18 @@ function activateCorde(key){
     }, 1000/vib_fps);
     
     //play the sound
-    note = 0;
-    adsr.active = true;
+    //note = key*2;
+    note = tuning_rel[key - 1];
+    adsr.active = true; //basic envelope
+    calculateADSR();
     a = buildSound(note, 'shape', adsr.master, vib_duration, adsr, false);
     a.play();
 }
 
 $(document).ready(function(){
-  drawNeck(6, 'guitar', Math.random()*1, 2);
+ // drawNeck(6, 'guitar', Math.random()*1, 2);
+  
+  
   // setInterval(update, 1000/30);
   $('a.key.num').bind('click touch', function(){
     console.log('h');
@@ -121,6 +165,27 @@ $(document).ready(function(){
     
     
 });
+
+
+
+function slugify(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+  
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;#";
+  var to   = "aaaaeeeeiiiioooouuuunc-------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
+
 
 
 
